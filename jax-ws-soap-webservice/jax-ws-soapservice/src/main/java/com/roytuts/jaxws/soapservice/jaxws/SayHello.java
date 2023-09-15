1347da8e2b9ccfef0c5a989a2155a2b10db99fe3
Soumitra <projwebinfo@gmail.com>
2023-09-15 21:03:02 +0530

    Create SayHello.java

---
+++ jax-ws-soap-webservice/jax-ws-soapservice/src/main/java/com/roytuts/jaxws/soapservice/jaxws/SayHello.java
@@ -0,0 +1,15 @@
+
+package com.roytuts.jaxws.soapservice.jaxws;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "sayHello", namespace = "http://soapservice.jaxws.roytuts.com/")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "sayHello", namespace = "http://soapservice.jaxws.roytuts.com/")
+public class SayHello {
+
+
+}
